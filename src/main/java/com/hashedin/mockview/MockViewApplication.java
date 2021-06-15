d02e24db918e821a9d0b38aea1e9894cb017ae7f
Kumar, Kshitij <kshitkumar@deloitte.com>
2021-06-15 16:39:13 +0530

    Initial Commit

---
+++ src/main/java/com/hashedin/mockview/MockViewApplication.java
@@ -0,0 +1,13 @@
+package com.hashedin.mockview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashedin/mockview/MockViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashedin.mockview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
